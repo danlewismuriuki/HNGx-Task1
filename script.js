3c4e7307a64da789d95b2b51810c064a5cdadf47
Dan <danlewismuriuki2@gmail.com>
2023-09-09 14:51:48 -0700

    Added Time in Milliseconds

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const d = new Date();
-document.getElementById("currentUTCTime").innerHTML = d;
-
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 const weekday = new Date();
 let day = days[weekday.getDay()];
 document.getElementById("currentDayOfTheWeek").innerHTML = day;
+
+
+const now = new Date();
+
+// Get the UTC milliseconds component of the current time
+const milliseconds = now.getUTCMilliseconds();
+document.getElementById("currentUTCTime").innerHTML = milliseconds;
+
