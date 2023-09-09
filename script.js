65391f2da1f56a12e84890b7f3f1b0564ab4608b
Dan <danlewismuriuki2@gmail.com>
2023-09-09 14:24:48 -0700

    Added date and time

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function displayDate(){
-    document.getElementById("current-day").innerHTML=Date();
-} 
+const d = new Date();
+document.getElementById("currentUTCTime").innerHTML = d;
+
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const weekday = new Date();
+let day = days[weekday.getDay()];
+document.getElementById("currentDayOfTheWeek").innerHTML = day;
